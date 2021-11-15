e268da86a2c84c8da3b5cda694c3a22252b0529f
Eric Liu <esliu3@illinois.edu>
2021-11-15 01:09:02 -0600

    debugged

--- src/main.cc
+++ src/main.cc
@@ -3,4 +3,5 @@
 
 int main() {
     //ProcessFile(); 
+    SentimentAnalyzer s; 
 }
--- src/sentiment_analyzer.cc
+++ src/sentiment_analyzer.cc
@@ -1,22 +1,21 @@
 #include "sentiment_analyzer.hpp"
 #include <fstream>
 #include <sstream> 
+#include <iostream>
 
 SentimentAnalyzer::SentimentAnalyzer(){
     ProcessLexicon(); 
 }
 
 void SentimentAnalyzer::ProcessLexicon(){
-    ifstream ifs("sentiment_lexicon.txt"); 
+    ifstream ifs("data/sentiment_lexicon.txt"); 
     string line;
     while(getline(ifs, line, '\n')){
         stringstream line_stream(line); 
         string keyword, value_string; 
         getline(line_stream, keyword, '\t'); 
         getline(line_stream, value_string, '\t');
-
         int value = stoi(value_string); 
-
         lex.insert(std::make_pair(keyword, value)); 
     }
 }
