db1b90fd2aee898999a655dcb9b6a37b22111844
Yifan Yin <yifany10@illinois.edu>
2021-11-19 19:21:50 +0000

    commit

--- src/main.cc
+++ src/main.cc
@@ -28,5 +28,5 @@ int main(int argc, char* argv[]) {
         myfile.close();
     }
     SentimentAnalyzer s; 
-    std::cout << s.Analyze(to_anlyze);
+    std::cout << "result: " << s.Analyze(to_anlyze) << std::endl;
 }
